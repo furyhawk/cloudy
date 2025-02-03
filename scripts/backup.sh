24f0c918ad32faa219a6eda280e7849c511bff93
furyhawk <furyx@hotmail.com>
2025-02-03 19:32:30 +0800

    fix: comment out jellyfin config export in backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,5 @@
 ./vackup.sh export core_traefik-public-certificates core_traefik-public-certificates.tar.gz
-./vackup.sh export apps_jellyfin_config apps_jellyfin_config.tar.gz
+# ./vackup.sh export apps_jellyfin_config apps_jellyfin_config.tar.gz
 ./vackup.sh export docmost_docmost_redis_data docmost_docmost_redis_data.tar.gz
 ./vackup.sh export openwebui_open_webui openwebui_open_webui.tar.gz
 ./vackup.sh export outline_outline_redis_data outline_outline_redis_data.tar.gz
