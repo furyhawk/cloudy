b096120482c247e323748590b20072c62de638cd
furyhawk <furyx@hotmail.com>
2026-04-11 15:08:10 +0800

    fix: update backup script to include additional exports and correct glance volume mapping

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,9 +1,16 @@
 ./vackup.sh export core_traefik-public-certificates core_traefik-public-certificates.tar.gz
 # ./vackup.sh export apps_jellyfin_config apps_jellyfin_config.tar.gz
 ./vackup.sh export docmost_docmost_redis_data docmost_docmost_redis_data.tar.gz
+./vackup.sh export docmost_docmost_redis_storage docmost_docmost_redis_storage.tar.gz
 ./vackup.sh export openwebui_open_webui openwebui_open_webui.tar.gz
+./vackup.sh export openwebui_chromadb openwebui_chromadb.tar.gz
 ./vackup.sh export outline_outline_redis_data outline_outline_redis_data.tar.gz
 ./vackup.sh export portainer_portainer-data portainer_portainer-data.tar.gz
 ./vackup.sh export searxng_searxng_data searxng_searxng_data.tar.gz
 ./vackup.sh export searxng_valkey_data2 searxng_valkey_data2.tar.gz
 ./vackup.sh export services_postgres_data services_postgres_data.tar.gz
+./vackup.sh export services_minio_data services_minio_data.tar.gz
+./vackup.sh export nextcloud_db nextcloud_db.tar.gz
+./vackup.sh export nextcloud_nextcloud nextcloud_nextcloud.tar.gz
+./vackup.sh export authentik_redis authentik_redis.tar.gz
+./vackup.sh export esphome_esphome_config esphome_esphome_config.tar.gz
